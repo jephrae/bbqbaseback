e9b686f3859658bcf062742e035683cbff4bbb88
jephrae <jeff.simons@gmail.com>
2021-02-26 16:46:23 -0600

    models made and migrated

--- bbqbase/models.py
+++ bbqbase/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
 # Create your models here.
+
+class Recipe(models.Model):
+    name = models.CharField(max_length=100)
+    # meattype = models.CharField(max_length=100)
+    CHICKEN = 'CK'
+    BEEF = 'BF'
+    PORK = 'PK'
+    FISH = 'FH'
+    GAME = 'GM'
+    MEAT_TYPE_CHOICES =[
+        (CHICKEN, 'Chicken'),
+        (BEEF, 'Beef'),
+        (PORK, 'Pork'),
+        (FISH, 'Fish'),
+        (GAME, 'Game'),
+    ]
+    meat_type = models.CharField(
+        max_length =2,
+        choices=MEAT_TYPE_CHOICES,
+        default=BEEF,
+    )
+    meat_cut = models.CharField(max_length=100)
+    cooktime = models.CharField(max_length=100)
+    temp = models.CharField(max_length=100)
+    credit = models.CharField(max_length=100)
+    photo_url = models.TextField()
+    desc = models.TextField()
+    instructions = models.TextField()
+
+    def __str__(self):
+        return self.name
